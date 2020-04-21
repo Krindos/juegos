abd59db512b834ac8b9b0483cdae42bfcf6a8719
Krindos <fomarco@gmail.com>
2020-04-21 19:08:39 -0300

    creacion de modelo.js y de los elem. principales

---
+++ js/modelo.js
@@ -0,0 +1,25 @@
+// ficha es el objeto usado en el juego Ta-Te-Ti para jugar
+var ficha={
+    jugador,
+    img,
+    tiempo
+}
+
+// Cada uno de los objetos usados en el juego Pares
+var carta={
+    jugador,
+    img,
+    posicionPropia,
+    posicionPar,
+    activa
+}
+
+// Para personalizar cada juego
+var jugador={
+    jugador,
+    nombre,
+    color,
+    ficha,
+    activo,
+    tiempo
+}
